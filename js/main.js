a8a50a7c3cb93a69c4805372411feee13749793b
WebCoders-503 <121188536+WebCoders-503@users.noreply.github.com>
2022-12-25 05:38:32 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -73,3 +73,78 @@ $(document).ready(function(){
 
 
 })
+
+
+
+var a;
+function show(){
+    if(a==1){
+        document.getElementById('accordion').style.display='none';
+        document.getElementById('up').innerHTML='<i class="fa-solid fa-chevron-up"></i>';
+        return a=0;
+    }
+    else{
+        document.getElementById('accordion').style.display='block';
+        document.getElementById('up').innerHTML='<i class="fa-solid fa-chevron-up"></i>';
+        return a=1;
+    }
+}
+function showI(){
+    if(a==1){
+        document.getElementById('accordion-1').style.display='none';
+        document.getElementById('up1').innerHTML='<i class="fa-solid fa-chevron-up"></i>';
+        return a=0;
+    }
+    else{
+        document.getElementById('accordion-1').style.display='block';
+        document.getElementById('up1').innerHTML='<i class="fa-solid fa-chevron-up"></i>';
+        return a=1;
+    }
+}
+
+
+function showIi(){
+    if(a==1){
+        document.getElementById('accordion-2').style.display='none';
+        document.getElementById('up1').innerHTML='<i class="fa-solid fa-chevron-up"></i>';
+        return a=0;
+    }
+    else{
+        document.getElementById('accordion-2').style.display='block';
+        document.getElementById('up1').innerHTML='<i class="fa-solid fa-chevron-up"></i>';
+        return a=1;
+    }
+}
+
+
+
+function showIii(){
+    if(a==1){
+        document.getElementById('accordion-3').style.display='none';
+        document.getElementById('up1').innerHTML='<i class="fa-solid fa-chevron-up"></i>';
+        return a=0;
+    }
+    else{
+        document.getElementById('accordion-3').style.display='block';
+        document.getElementById('up1').innerHTML='<i class="fa-solid fa-chevron-up"></i>';
+        return a=1;
+    }
+}
+
+
+
+
+function showIiii(){
+    if(a==1){
+        document.getElementById('accordion-4').style.display='none';
+        document.getElementById('up1').innerHTML='<i class="fa-solid fa-chevron-up"></i>';
+        return a=0;
+    }
+    else{
+        document.getElementById('accordion-4').style.display='block';
+        document.getElementById('up1').innerHTML='<i class="fa-solid fa-chevron-up"></i>';
+        return a=1;
+    }
+}
+
+
