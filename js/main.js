919f65f5d1d5b947f1a87f38a89a7fd49a92823f
shakib53626 <107703379+shakib53626@users.noreply.github.com>
2022-12-28 23:10:00 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -69,13 +69,74 @@ $(document).ready(function(){
         $('#train span').removeClass('line');
         $('#flight span').addClass('line');
     })
-
-    
-  
+})
 
 
+$(document).ready(function(){
+
+
+    $('.login-form').hide();
+    $('.singup-f').show();
+
+
+    $('#btn-singup span').addClass('sing-up');
+
+    $('#btn-login').click(function(){
+
+
+        $('.login-form').show();
+        $('.singup-f').hide();
+
+
+        $('#btn-login span').addClass('sing-up');
+        $('#btn-singup span').removeClass('sing-up');
+    })
+
+    $('#btn-singup').click(function(){
+
+
+        $('.login-form').hide();
+        $('.singup-f').show();
+
+
+        $('#btn-login span').removeClass('sing-up');
+        $('#btn-singup span').addClass('sing-up');
+    })
+    $('#login').click(function(){
+        $('.login-box').show();
+        $('.singup-f').hide();
+        $('.login-form').show();
+        $('#btn-login span').addClass('sing-up');
+        $('#btn-singup span').removeClass('sing-up');
+    })
+    $('#singin').click(function(){
+        $('.login-box').show();
+        $('.singup-f').show();
+        $('.login-form').hide();
+        $('#btn-login span').removeClass('sing-up');
+        $('#btn-singup span').addClass('sing-up');
+    })
+    $('#not-member-singup').click(function(){
+        $('.singup-f').show();
+        $('.login-form').hide();
+        $('#btn-login span').removeClass('sing-up');
+        $('#btn-singup span').addClass('sing-up');
+    })
 
 })
+
+var login = document.getElementById('login');
+var singin = document.getElementById('singin');
+var notMemberSingup = document.getElementById('not-member-singup');
+    login.addEventListener('click', function(){
+        document.getElementById('login-box').style.display='flex';
+    })
+    singin.addEventListener('click', function(){
+        document.getElementById('login-box').style.display='flex';
+    })
+    notMemberSingup.addEventListener('click', function(){
+        document.getElementById('login-box').style.display='flex';
+    })
 
 
 
@@ -154,16 +215,7 @@ function showIiii(){
 
 
 
-var card = document.getElementById("card");
-
-		function openRegister(){
-			card.style.transform ="rotateY(-180deg)";
-		}
-		function openLogein(){
-			card.style.transform ="rotateY(0deg)";
-		}
 
         function sideBox(){
             document.getElementById('menubox').style.display='block';
-            document.getElementById('menubox').style.opacity='1';
         }
